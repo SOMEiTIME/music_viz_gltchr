e9b921f4f65c301c3343107ba3eaa27c83168282
Benjamin <45668639+SOMEiTIME@users.noreply.github.com>
2023-06-21 15:20:10 +0700

    Commit - not using a blopFile approach

--- script.js
+++ script.js
@@ -32,32 +32,52 @@ const bufferLength = analyser.frequencyBinCount;
 const soundDataArray = new Uint8Array(bufferLength);
 const barWidth = canvas.width / bufferLength;
 
-const fps = 60;
+const fps = 120;
 
 //animate
 let x = 0;
 function animate() {
   x = 0;
   offscreenContext.drawImage(video1, 0, 0, canvas.width, canvas.height)
+  analyser.getByteFrequencyData(soundDataArray);
+
+  /*
+  //processes the entire image, shifting the colors
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   let frame = offscreenContext.getImageData(0, 0, canvas.width, canvas.height);
   const frameData = frame.data;
 
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   analyser.getByteFrequencyData(soundDataArray);
-  
   for (let i = 0; i < frameData.length; i += 1) {
     barHeight = soundDataArray[Math.round(bufferLength / i)];
     frameData[i] = frameData[i + barHeight] + barHeight * .5;
     i = i + 1;
   }
-
-  canvasContext.putImageData(frame, 0, 0);
+  canvasContext.putImageData(frame, 0, 0); 
+  */
+  canvasContext.fillRect("black", 0, 0, canvas.width, canvas.height);
 
   for (let i = 0; i < bufferLength; i++) {
     barHeight = soundDataArray[i];
-    canvasContext.fillStyle = "grey";
+    if (barHeight == 0) {
+      barHeight = 1;
+    }
 
-    canvasContext.fillRect(x, canvas.height - 2 * barHeight, barWidth - barWidth/3, barHeight);
+    let frame = offscreenContext.getImageData(x, 0 + barHeight * .1, barWidth - barHeight * .1, canvas.height);
+    let frameData = frame.data;
+    for (let k = 0; k < frameData.length; k += 4) {
+      let newVal = frameData[k + barHeight] + barHeight * .3;
+      if (newVal > 250 || newVal < 50) {
+        newVal = 0;
+      }
+      frameData[k] = newVal
+    }
+
+    canvasContext.putImageData(frame, x + barHeight * .01, 0);
+    canvasContext.putImageData(frame, x, canvas.height - 2 * barHeight);
+    //draw rectangles
+    canvasContext.fillStyle = "white";
+    //canvasContext.fillRect(x, canvas.height - 2 * barHeight, barWidth - barWidth/3, barHeight);
     x += barWidth;
   }
 
